fbc8142a42586729c4dbe7444cac738e807719d9
nounamenomane <nounamenomane@gmail.com>
2023-10-12 00:21:58 +0600

    slider in discounts block, adapt slider in discount block, adapt rieltors block, adapt servises block

--- src/componets/discountsBLock/DiscountsBLock.jsx
+++ src/componets/discountsBLock/DiscountsBLock.jsx
@@ -1,40 +1,73 @@
 import React from "react";
 import scss from "./DiscountsBLock.module.scss";
 import CardDiscout from "../cardDiscount/CardDiscout";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { CustomPrevArrow, CustomNextArrow } from "./ButtonSLider";
 
 function DiscountsBLock() {
-  const responsive = {
-    0: { items: 1 },
-    576: { items: 1 },
-    768: { items: 2 },
-    992: { items: 3 },
-    1300: { items: 4 },
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-  const cards = (text, logo) => {
-    return (
-      <div className={scss.cards}>
-        <CardDiscout text={text} logo={logo} />
-      </div>
-    );
-  };
+  const card = [
+    { text: "Terraguard", logo: "./assets/images/logoterra.png" },
+    { text: "Primex", logo: "./assets/images/logo primex.png" },
+    { text: "Starforge", logo: "./assets/images/logo star.png" },
+    { text: "ProBuilding", logo: "./assets/images/logo pro.png" },
+  ];
+  const duplicatedCards = card.concat(card);
+
   return (
     <div className={scss.discountsBLock}>
       <h1>Акции и скидки на новостройки</h1>
-      <AliceCarousel
-        autoPlay
-        autoPlayInterval={2000}
-        buttonsDisabled={true}
-        responsive={responsive}
-        infinite={true}
-      >
-        {cards("Terraguard", "./assets/images/logoterra.png")}
-        {cards("Primex", "./assets/images/logo primex.png")}
-        {cards("Starforge", "./assets/images/logo star.png")}
-        {cards("ProBuilding", "./assets/images/logo pro.png")}
-      </AliceCarousel>
+      <Slider {...settings}>
+        {duplicatedCards.map((card, index) => (
+          <div key={index} className={scss.cards}>
+            <CardDiscout text={card.text} logo={card.logo} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
