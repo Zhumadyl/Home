cd6c0e67ee45fdaf0355075e33ea4db24e68802c
Bekzat247 <toktobekzat@gmail.com>
2023-11-01 15:08:33 +0600

    feat: add translation

--- src/componets/GalleryCreatePage/GalleryCreatePage.jsx
+++ src/componets/GalleryCreatePage/GalleryCreatePage.jsx
@@ -1,28 +1,30 @@
+import { useTranslation } from 'react-i18next';
 import scss from './GalleryCretaePage.module.scss'
 
 function GalleryCreatePage() {
+    const { t } = useTranslation();
   return (
     <div className={scss.wrapper}>
-        <h2>Галерея</h2>
+        <h2>{t("createPage.GalleryCreatePage.h2")}</h2>
         <div className={scss.GalleryBlock}>
-            <button><img src="./et_download.svg" alt="" />Загрузить фотографии</button>
-            <p>или перетащите их сюда </p>
+            <button><img src="./et_download.svg" alt="" />{t("createPage.GalleryCreatePage.btn")}</button>
+            <p>{t("createPage.GalleryCreatePage.ordragthem")} </p>
         </div>
         <div>
             <div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
-                <p>Главное фото</p>
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
+                <p>{t('createPage.GalleryCreatePage.mainImage')}</p>
             </div>
             <div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
             </div><div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
             </div><div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
             </div><div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
             </div><div>
-                <img src="./fluent_image-sparkle-20-regular.svg" alt="" />
+                <img src="./fluent_image-sparkle-20-regular (1).svg" alt="" />
             </div>
         </div>
     </div>
